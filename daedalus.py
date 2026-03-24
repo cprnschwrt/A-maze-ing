cc22d7eeace1bb571d499bde4266bab53288baa0
Bastien Gix <bgix@k1r3p1.42mulhouse.fr>
2026-03-24 14:17:57 +0100

    norm check and removed unwanted_files

--- daedalus.py
+++
@@ -1,9 +0,0 @@
-cell_template = {"N": True, "E": True, "S": True, "W": True}
-
-def create_empty_grid(width, height):
-    return [[cell_template.copy() for x in range(width)] for y in range(height)]
-
-def iterate_grid(grid):
-    for y in range(len(grid)):
-        for x in range(len(grid[0])):
-            cell = grid[y][x]
--- utils/constants.py
+++
@@ -1,9 +0,0 @@
-# utils/constants.py
-
-pattern_height = 3
-pattern_width = 3
-pattern = [
-    "C04084",
-    "C084C0",
-    "000009"
-]
